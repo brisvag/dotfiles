135f0285f07a2fcb68a2cc26fb75ed9aada690f9
Lorenzo Gaifas <brisvag@gmail.com>
2021-11-24 16:25:08 +0100

    work: new monitor setup

---
+++ .config/xrandr/screen_layout.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+xrandr \
+    --output DVI-I-0 \
+    --off \
+    --output DVI-I-1 \
+    --mode 1920x1200 \
+    --pos 0x0 \
+    --rotate left \
+    --output DP-0 \
+    --off \
+    --output DP-1 \
+    --off \
+    --output DP-2 \
+    --mode 1920x1080 \
+    --pos 3120x324 \
+    --rotate normal \
+    --output DP-3 \
+    --mode 1920x1080 \
+    --pos 1200x324 \
+    --rotate normal \
+    --output DVI-I-1-0 \
+    --off \
+    --output DVI-I-1-1 \
+    --off \
+    --output DP-1-0 \
+    --off \
+    --output DP-1-1 \
+    --off \
+    --output DP-1-2 \
+    --off \
+    --output DP-1-3 \
+    --off
