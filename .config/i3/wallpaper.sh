4ff291ead417dcfc8553b1db2ab4b6672e429310
Lorenzo Gaifas <brisvag@gmail.com>
2022-04-11 16:18:45 +0200

    home: single screen in wallpaper

--- .config/i3/wallpaper.sh
+++ .config/i3/wallpaper.sh
@@ -3,6 +3,6 @@
 while true; do
     pscircle --output=/tmp/wp_pscircle.png
     sleep 1
-    feh --no-fehbg --bg-fill ~/pictures/arch-linux-wallpaper.png --bg-fill ~/pictures/arch-linux-wallpaper.png --bg-fill /tmp/wp_pscircle.png
+    feh --no-fehbg --bg-fill /tmp/wp_pscircle.png
     sleep 10
 done
