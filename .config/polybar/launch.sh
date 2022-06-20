2cbc72f5dd6ee4f5508c018928d310ab40691cea
Lorenzo Gaifas <brisvag@gmail.com>
2022-06-20 15:07:39 +0200

    all: add calendar, fix whatsapp, fix tray

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,12 +7,17 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bars
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload bottom &
-  done
-else
-  polybar --reload bottom &
-fi
+# split on newline for loop
+IFS=$'\n'
+monitors=$(xrandr --query | grep " connected")
+for monitor in $monitors; do
+    m=$(echo $monitor | cut -d" " -f1)
+    if [[ "$monitor" == *primary* ]]; then
+        tray="right"
+    else
+        tray='none'
+    fi
+    MONITOR=$m TRAY=$tray polybar --reload bottom &
+done
 
 echo "Bars launched..."
