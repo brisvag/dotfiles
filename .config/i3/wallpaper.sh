e8777c4751803200c9a4f205ba7da1b5b00f512e
Lorenzo Gaifas <brisvag@gmail.com>
2023-07-04 23:39:36 +0200

    home: laptop stuff that needed updates due to scaling

--- .config/i3/wallpaper.sh
+++ .config/i3/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    pscircle --output=/tmp/wp_pscircle.png
+    pscircle --output=/tmp/wp_pscircle.png  --output-width=2560 --output-height=1600 --collapse-threads=1 --tree-center=-100.0:0.0 --tree-font-face=JuliaMono --toplists-font-face=JuliaMono --cpulist-center=800.0:-100.0 --memlist-center=800.0:100.0
     sleep 1
     feh --no-fehbg --bg-fill /tmp/wp_pscircle.png
     sleep 3
--- .profile
+++ .profile
@@ -1,6 +1,12 @@
 # fix local
 TZ='Europe/Paris'; export TZ
 
+# fix scaling for hidpi (together with xresources)
+# export GTK_SCALE=1
+# export GTK_DPI_SCALE=0.85
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_SCREEN_SCALE_FACTORS=2
+
 # paths and defaults
 export MANPATH="/usr/local/man:$MANPATH"
 export LANG=en_US.UTF-8
