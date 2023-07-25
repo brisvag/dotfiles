27f3ac4bddcd53d70631a567281acd55f34b6ba6
Lorenzo Gaifas <brisvag@gmail.com>
2023-07-25 22:13:42 +0200

    home: fix dpi stuff

--- .config/xrandr/screen_layout.sh
+++ .config/xrandr/screen_layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-4 --primary --mode 2560x1600 --pos 0x0 --rotate normal --rate 165.00 --dpi 178
+xrandr --output DP-4 --primary --mode 2560x1600 --pos 0x0 --rotate normal --rate 165.00 --dpi 96
