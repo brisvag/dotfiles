bef4be5981f471eba9d4b325090a3cd36f318f03
Lorenzo Gaifas <brisvag@gmail.com>
2023-08-16 15:30:49 +0200

    work: fix font and bg

--- .config/i3/wallpaper.sh
+++ .config/i3/wallpaper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while true; do
-    pscircle --output=/tmp/wp_pscircle.png  --output-width=1920 --output-height=1080 --collapse-threads=1 --tree-center=-100.0:0.0 --tree-font-face="MesloLGS NF" --toplists-font-face="MesloLGS NF" --cpulist-center=800.0:-100.0 --memlist-center=800.0:100.0
+    pscircle --output=/tmp/wp_pscircle.png --collapse-threads=1 --tree-center=-100.0:0.0 --tree-font-face="MesloLGS NF" --toplists-font-face="MesloLGS NF" --cpulist-center=800.0:-100.0 --memlist-center=800.0:100.0
     sleep 1
-    feh --no-fehbg --bg-fill /tmp/wp_pscircle.png
+    feh --no-fehbg --bg-fill ~/pictures/arch-linux-wallpaper.png --bg-fill ~/pictures/arch-linux-wallpaper.png --bg-fill /tmp/wp_pscircle.png
     sleep 3
 done
